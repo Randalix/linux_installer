7669c7056d5ce99ea4fc5f447e19d5e80367960a
Randalix <j.jheintz.90@gmail.com>
2022-08-02 20:58:40 +0200

    modified:   location.sh

--- location.sh
+++ location.sh
@@ -7,5 +7,5 @@ echo "" > /etc/locale.gen
 echo "de_DE.UTF-8 UTF-8">> /etc/locale.gen
 echo "de_DE ISO-8859-1">> /etc/locale.gen
 echo "de_DE@euro ISO-8859-15">> /etc/locale.gen
-locale-gen
-printf "KEYMAP=%s" de >> /etc/vconsole.conf
+locale-gen de_DE.UTF-8
+printf "KEYMAP=%s" de > /etc/vconsole.conf
