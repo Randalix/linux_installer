75d11017329bb3e7d8572946823fc8f23e4d349e
Randalix <j.jheintz.90@gmail.com>
2022-08-02 17:32:21 +0200

    add user

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
-read -p "New User Name: " user
-read -p "Password: " passwd
 apt install openssh-server git
+read -p "Machine Name" mymachine
+read -p "New User Name: " myusr
+read -p "Password: " passwd
+printf "%s" $myusr > myusr
+printf "%s" $passwd > mypass
+printf "%s" $mymachine > mymachine
+printf "%s" $passwd | passwd &> /dev/null
+useradd -m -c $myusr -s /bin/bash $myusr
+printf "%s" $passwd | passwd $myusr &> /dev/null
+usermod -aG sudo $myusr
+
