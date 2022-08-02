90580601568c91764b254a03234bcd6de109b223
Randalix <j.jheintz.90@gmail.com>
2022-08-02 20:45:53 +0200

    modified:   readme.md 	modified:   sudo.sh

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
-cd /urs/src
-
+su
+cd
+mkdir -p src
+cd src
 git clone https://github.com/Randalix/linux_installer.git
+cd linux_installer
+./setup.sh
--- sudo.sh
+++ sudo.sh
@@ -7,7 +7,7 @@ myusr=$(cat myusr)
 mymachine=$(cat myusr)
 
 cp /etc/sudoers /etc/sudoers.tmp
-sed -i  's/No Password Rules/{N;d;}g' /etc/sudoers.tmp
+sed -i  "s/\#\ No\ Password\ Rules/{N;d;}g" /etc/sudoers.tmp
 printf "%s\n%s %s" "# No Password Rules" $myusr "$(cat ./nopass)" > /etc/sudoers.tmp
 visudo -c -f /tmp/sudoers.new
 if [ "$?" -eq "0" ]; then
