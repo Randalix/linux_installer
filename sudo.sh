49969db5e064521b5ca00b8ba2473f00e8d87b57
Randalix <j.jheintz.90@gmail.com>
2022-08-03 11:54:44 +0200

    modified:   sudo.sh

--- sudo.sh
+++ sudo.sh
@@ -3,11 +3,16 @@
 install="$(cat ./install)"
 update="$(cat ./update)"
 $update
+FILE=myusr
+if [ ! -f "$FILE" ]; then
+    ./init.sh
+fi
+
 mypass=$(cat mypass)
 myusr=$(cat myusr)
 mymachine=$(cat myusr)
 
-sudo cp /etc/sudoers /tmp/sudoers.tmp
+sudo cp -r /etc/sudoers /tmp/sudoers.tmp
 sudo bash -c 'printf "%s\n%s %s" "# No Password Rules" $myusr "$(cat ./nopass)" >> /tmp/sudoers.tmp'
 #sudo printf "%s\n%s %s" "# No Password Rules" $myusr "$(cat ./nopass)" >> /tmp/sudoers.tmp
 
