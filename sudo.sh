295b092861d0dc91ba1fe20e81b05418321c2f4c
Randalix <j.jheintz.90@gmail.com>
2022-08-03 11:48:09 +0200

    modified:   sudo.sh

--- sudo.sh
+++ sudo.sh
@@ -7,12 +7,13 @@ mypass=$(cat mypass)
 myusr=$(cat myusr)
 mymachine=$(cat myusr)
 
-cp /etc/sudoers /etc/sudoers.tmp
-sed -i  "s/\#\ No\ Password\ Rules/{N;d;}g" /etc/sudoers.tmp
-printf "%s\n%s %s" "# No Password Rules" $myusr "$(cat ./nopass)" > /etc/sudoers.tmp
-visudo -c -f /tmp/sudoers.new
+cp /etc/sudoers /tmp/sudoers.tmp
+#sed -i  "s/# No\ Password Rules/{N;d;}/g" /etc/sudoers.tmp
+sudo printf "%s\n%s %s" "# No Password Rules" $myusr "$(cat ./nopass)" >> /tmp/sudoers.tmp
+printf "\n%s\ssword Rules" "$myusr" "$(cat ./nopass)" > /tmp/sudoers.tmp
+visudo -c -f /tmp/sudoers.tmp
 if [ "$?" -eq "0" ]; then
-    cp /tmp/sudoers.new /etc/sudoers
+    cp /tmp/sudoers.tmp /etc/sudoers
 fi
-rm /etc/sudoers.tmp
+rm /tmp/sudoers.tmp
 
