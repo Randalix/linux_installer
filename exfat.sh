1d0c4adc4c715768c2d780bc6b8cd047d49e84dc
Randalix <j.heintz.90@gmail.com>
2022-08-03 15:51:11 +0200

    geändert:       exfat.sh

--- exfat.sh
+++ exfat.sh
@@ -3,5 +3,7 @@ install="$(cat ./install)"
 $install exfat-fuse
 drive=$(lsblk -o +FSTYPE | grep exfat | awk '{NF=1}1' | cut -c3- | fzf)
 read -p "Mountpoint : " mnt
+user=$(uid)
+group=$(gid)
 sudo mkdir $mnt
-sudo mount -t exfat -o rw,users,uid=1001,gid=1001,dmask=007,fmask=117 /dev/$drive $mnt
+sudo mount -t exfat -o rw,users,uid=$uuid,gid=$group,dmask=007,fmask=117 /dev/$drive $mnt
