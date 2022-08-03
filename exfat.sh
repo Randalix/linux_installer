8c60cb765da4de2720abd4e6e791601f84d999a0
Randalix <j.heintz.90@gmail.com>
2022-08-03 15:52:34 +0200

    geändert:       exfat.sh

--- exfat.sh
+++ exfat.sh
@@ -5,5 +5,6 @@ drive=$(lsblk -o +FSTYPE | grep exfat | awk '{NF=1}1' | cut -c3- | fzf)
 read -p "Mountpoint : " mnt
 user=$(uid)
 group=$(gid)
+cmd="mount -t exfat -o rw,users,uid=$uuid,gid=$group,dmask=007,fmask=117 /dev/$drive $mnt"
 sudo mkdir $mnt
-sudo mount -t exfat -o rw,users,uid=$uuid,gid=$group,dmask=007,fmask=117 /dev/$drive $mnt
+sudo $cmd
