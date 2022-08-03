184a32aa5f8b20f3cab1622c364a780c32088d9b
Randalix <j.jheintz.90@gmail.com>
2022-08-03 04:02:45 +0200

    modified:   machine.sh

--- machine.sh
+++ machine.sh
@@ -8,7 +8,8 @@ mymachine=$(cat mymachine)
 
 echo "$mymachine" > /etc/hostname
 echo "" >> /etc/hosts
-echo -e "127.0.0.1\tlocalhost" >> /etc/hosts
+echo -e "127.0.0.1\tlocalhost" > /etc/hosts
 echo -e "::1\t\tlocalhost" >> /etc/hosts
-echo -e "127.0.1.1\t$mymachine.localdomain\t$mymachine" >> /etc/hosts
-echo -e "127.0.1.1\t$mymachine.localdomain\t$mymachine" >> /etc/hosts
+echo -e "127.0.1.1\t\t$mymachine" >> /etc/hosts
+#echo -e "127.0.1.1\t$mymachine.localdomain\t$mymachine" >> /etc/hosts
+#echo -e "127.0.1.1\t$mymachine.localdomain\t$mymachine" >> /etc/hosts
