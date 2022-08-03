85cc5c302ac4e2bbdc1c2e272d863c56fb17afcc
Randalix <j.heintz.90@gmail.com>
2022-08-03 15:32:39 +0200

    geändert:       ntfs.sh

--- ntfs.sh
+++ ntfs.sh
@@ -3,4 +3,5 @@ install="$(cat ./install)"
 $install ntfs-3g
 drive=$(lsblk -o +FSTYPE | grep ntfs | awk '{NF=1}1' | cut -c3- | fzf)
 read -p "Mountpoint [/mnt/usb]: " mnt
+sudo mkdir $mnt
 sudo mount -t ntfs-3g /dev/$drive $mnt
