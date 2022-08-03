08302b0ddc79a360a8cfc85d5b52fc5d5b509f69
Randalix <j.heintz.90@gmail.com>
2022-08-03 15:23:05 +0200

    geändert:       nala.sh neue Datei:     ntfs.sh

--- nala.sh
+++ nala.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 install="$(cat ./install)"
 $install nala
 nala --install-completion bash
---
+++ ntfs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+install="$(cat ./install)"
+$install ntfs-3g
+drive=$(lsblk -o +FSTYPE | grep ntfs | awk '{NF=1}1' | cut -c3- | fzf)
+read -p "Mountpoint [/mnt/usb]: " mnt
+sudo mount -t ntfs-3g /dev/$drive $mnt
